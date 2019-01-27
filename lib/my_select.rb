bcd64bfbb2ba9c0860c66466beb277577f372ce9
Bryn Bennett <bryn.bennett@gmail.com>
2019-01-27 02:43:53 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,8 +4,8 @@ def my_select(array)
  
     while i < array.length
       new_array = []
-      if
-        new_array << yield(array[i])
+      if yield(array[i])
+        new_array << 
         i = i + 1
       end
     end
