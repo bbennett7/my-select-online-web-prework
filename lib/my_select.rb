8e17991ba6ed45880fef695d74d8374441abe11b
Bryn Bennett <bryn.bennett@gmail.com>
2019-01-27 02:51:12 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,12 +2,12 @@ def my_select(array)
  if block_given?
   i = 0
   new_array = []
-  while i < array.length 
-      if (yield(array[i]))
-        new_array << array[i] 
+    while i < array.length 
+        if (yield(array[i]))
+          new_array << array[i] 
+        end 
+        i += 1 
       end 
-      i += 1 
-    end 
  
     new_array
   else
